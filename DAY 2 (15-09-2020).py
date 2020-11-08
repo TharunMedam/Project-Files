1e0623e228d2fb4a0dd6b4e2b3f33d3f20172658
TharunMedam <74060463+TharunMedam@users.noreply.github.com>
2020-11-08 12:40:02 +0530

    Add files via upload

---
+++ DAY 2 (15-09-2020).py
@@ -0,0 +1,16 @@
+print("hello everyone")
+print("waelcome to python")
+print("sum=",55+77)
+print("dif=",66-33)
+print("mul=",10*5)
+num1=50
+num2=30
+print("before swapping",num1,num2)
+#temp=num2
+#num2-num1
+#num1=temp
+'''num1=num1+num2
+num2=num1-num2
+num1=num1-num2'''
+num1,num2=num2,num1
+print("after swapping",num1,num2)
